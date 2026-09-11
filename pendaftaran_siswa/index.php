28e4dbfc5afdc665c95f8840eacdbcadc4c593c8
Explorer-tech-risingcoder <kojekpermen0@gmail.com>
2026-09-12 06:36:39 +0700

    menambahkan tag h2 di index.php

--- pendaftaran_siswa/index.php
+++ pendaftaran_siswa/index.php
@@ -16,6 +16,7 @@
     <div class="container">
 
         <h2>Form Pendaftaran Siswa Baru</h2>
+        <h1> GITHUB </h1>
 
         <form action="simpan.php" method="POST" onsubmit="return validasiForm()">
 
